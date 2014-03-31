bb8df9537855a9851df294f2cae35f5c571d5348
Bjørn Remseth <rmz@comoyo.com>
2014-03-31 12:54:11 +0200

    Just doodling around

---
+++ mkeep/src/com/telenordigital/ctrlmeta/mkeep/Mkeep.java
@@ -0,0 +1,17 @@
+
+package com.telenordigital.ctrlmeta.mkeep;
+
+/**
+ *
+ * @author rmz
+ */
+public class Mkeep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
