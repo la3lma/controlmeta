9e991753075ad7917c6ceea01eb53861e0fd9b89
Bjørn Remseth <rmz@comoyo.com>
2014-04-01 08:39:15 +0200

    Fix syntax errors

--- mkeep/src/python/mkeep.py
+++ mkeep/src/python/mkeep.py
@@ -11,25 +11,30 @@ app = Flask(__name__)
 ### Media CRUD
 ###
 
-@app.route('/media/id/<id>/media', methods = ['GET'])
+@app.route('/media', methods = ['GET'])
 def get_media():
+    "Get a list of all the available media."
+    return Response(status=500)
+
+@app.route('/media/id/<id>/media', methods = ['GET'])
+def get_media(id):
     "Get the media representation of identified asset"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/media', methods = ['POST'])
 def post_media(id):
     "Write the media representation an identified asset"
-     Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>', methods = ['DELETE'])
-def delete_media_and_meta(id)
+def delete_media_and_meta(id):
     "Delete both media and metadata for an identified asset"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>', methods = ['DELETE'])
-def delete_media(id)
+def delete_media(id):
     "Delete both media for an identified asset"
-    Response(status=500)
+    return Response(status=500)
 
 
 ##
@@ -38,33 +43,32 @@ def delete_media(id)
 @app.route('/media/id/<id>/meta/<metatype>', methods = ['GET'])
 def get_meta_list(id, metatype):
     "Get list of metadata assets associated with a media asset"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/metaid/<metaid>', methods = ['GET'])
 def get_meta(id, metaid):
     "Get a particular metadata instance"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/metatype/<metatype>', methods = ['POST'])
 def post_new_meta(id, metatype):
     "Get list of metadata assets associated with a media asset"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/metaid/<metaid>', methods = ['POST'])
 def post_meta(id, metaid):
     "Post a particular metadata instance"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/metaid/<metaid>', methods = ['DELETE'])
 def delete_meta(id, metaid):
     "Get a particular metadata instance"
-    Response(status=500)
+    return Response(status=500)
 
 @app.route('/media/id/<id>/meta/<metatype>', methods = ['DELETE'])
-def delete_meta_for_id(id, metatype)
+def delete_meta_for_id(id, metatype):
     "Delete all metadata of a particular type for a specific media item"
-    Response(status=500)
-        
+    return Response(status=500)
     
 ###
 ###  Accessing the task queue
@@ -72,23 +76,22 @@ def delete_meta_for_id(id, metatype)
 
 @app.route('/task/type/<type>/waiting/next', methods = ['GET'])
 def get_next_waiting_task(type):
-        Response(status=500)
+    return Response(status=500)
         
 @app.route('/task/type/<type>/waiting/pick', methods = ['POST'])
 def pick_next_waiting_task(type):
-        Response(status=500)
+    return Response(status=500)
         
-@app.route('/task/type/<type>/in-progress/list', methods = ['GET'])
+@app.route('/task/type/<type>/in-progress', methods = ['GET'])
 def get_in_progress_task_list(type):
-        Response(status=500)
+    return Response(status=500)
         
-@app.route('/task/type/<type>/done/list', methods = ['POST']
+@app.route('/task/type/<type>/done', methods = ['POST'])
 def get_done_task_list(type):
-        Response(status=500)
+    return Response(status=500)
 
 
 ##  XXX Deletes are not added
 
 if __name__ == '__main__':
     app.run(debug = True)
-
