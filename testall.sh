b5782bd5214bf551bc07b25106acc395ce14de93
Bjørn Remseth <rmz@comoyo.com>
2014-04-10 14:09:35 +0200

    Rename empty_crud_tests to test_empty_crud

--- testall.sh
+++ testall.sh
@@ -2,7 +2,7 @@
 clear
 
 PYTHON=/usr/bin/python
-TESTS="empty_crud_tests.py \
+TESTS="test_empty_crud.py \
       test_media_and_meta_storage_directly.py \
       test_taskqueuestorage.py \
       media_usecases_test.py \
