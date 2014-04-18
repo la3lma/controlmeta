d681cdaa6755aa840a004111437bcbd983ea9d16
Bjørn Remseth <rmz@comoyo.com>
2014-04-18 23:09:54 +0200

    Add a test to check if the eb utility is isntalled before deploying

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@
 HOMEDIR=$(dirname  "$0")
 DEPLOYDIR="$HOMEDIR/../controlmeta.aws"
 
+if [  -z "$(type eb)" ]  ; then
+  echo "No 'eb' utility found in PATH variable, bailing out"
+  exit 1
+fi
+
 if [ !  -d "$DEPLOYDIR" ] ; then
   echo "Could not find  deployment directory ,$DEPLOYDIR', bailing our"
   exit 1
