2f8b4be78fac6b189c95b1b5d5894bc218aeebaa
Bjørn Remseth <rmz@comoyo.com>
2014-04-20 20:52:14 +0200

    An even better deployment script

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,19 @@ if [ !  -d "$DEPLOYDIR" ] ; then
   echo "Could not find  deployment directory ,$DEPLOYDIR', bailing our"
   exit 1
 fi
+
+# Nuke cruft
+"${HOMEDIR}/clean.sh"
+
+SUBDIRS="mediameta tasks"
 # First clean up the deploydir
-(cd $DEPLOYDIR && rm -f *.py)
+(cd $DEPLOYDIR && rm -f *.py $SUBDIRS)
 
 # Then copy all python scripts that are not for testing to the deploydir
 cp $(ls $HOMEDIR/*.py | grep -v test) "$DEPLOYDIR"
+cp -r $SUBDIRS "$DEPLOYDIR"
 
+(cd $DEPLOYDIR && git add $SUBDIRS)
 (cd $DEPLOYDIR && git commit -a -m "New version")
 
 # Finally push the new files to the application server
