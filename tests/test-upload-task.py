5c5c1b4106e40277a3c102a0db4ad7877fe84dac
Bjørn Remseth <rmz@comoyo.com>
2014-04-23 09:13:45 +0200

    More steps, still not there.

--- tests/test-upload-task.py
+++ tests/test-upload-task.py
@@ -14,11 +14,24 @@ class  ControlMetaClient:
     def upload_task(self, type, parameters):
         tasktypepath = "task/type/%s" % type
         posturl = "%s%s" %(base_url, tasktypepath)
-        print "posturl = ", posturl
         response = requests.post(posturl, data=json.dumps(parameters), headers=self.JSON_HEADERS)
-        print response
-        print response.text
+        # XXX Some checking of return values and perhaps throwing
+        #     of exceptions?
         return json.loads(response.text)
+
+    def pick_task(self, type, agent_id):
+        print "Picking."
+        agent_id={'agentId':agent_id}
+        pickurl="%stask/waiting/type/%s/pick" %(base_url, type)
+        pickresponse = requests.post(pickurl, data=json.dumps(agent_id), headers=json_headers)
+        return json.loads(pickresponse.text)
+
+    def declare_task_done(self, task_id, agent_id):
+        pickurl="%s%s%s" %(base_url, 'task/id/', task_id)
+        agent_id={'agentId': agent_id}
+        raw_response = requests.delete(pickurl, data=json.dumps(agent_id), headers=json_headers)
+        return raw_response
+
       
     
 ## Parsing command line parameters
@@ -32,8 +45,6 @@ cmclient = ControlMetaClient(base_url)
 print "Testing against base-url = ", base_url
 json_headers = {'content-type': 'application/json'}
 
-
-
 parameters={'parameter1':'Value 1'}
 data = cmclient.upload_task("face", parameters)
 task_id=data['taskId']
@@ -43,19 +54,21 @@ print "taskId=",task_id
 ## Then pick a task
 
 print "Picking."
-agent_id={'agentId':'007'}
-pickurl="%s%s" %(base_url, 'task/waiting/type/face/pick')
-pickresponse = requests.post(pickurl, data=json.dumps(agent_id), headers=json_headers)
-picked_task_id=json.loads(pickresponse.text)['taskId']
+# agent_id={'agentId':'007'}
+# pickurl="%s%s" %(base_url, 'task/waiting/type/face/pick')
+# pickresponse = requests.post(pickurl, data=json.dumps(agent_id), headers=json_headers)
+
+pickresponse=cmclient.pick_task('face', '007')
 
 print "pickresponse = ", pickresponse
-print "pickresponse.text = ", pickresponse.text
+picked_task_id=pickresponse['taskId']
 print "picked task id", picked_task_id
 
 
 ## And declare it as done
-pickurl="%s%s%s" %(base_url, 'task/id/', picked_task_id)
-deletedresponse= requests.delete(pickurl, data=json.dumps(agent_id), headers=json_headers)
+
+deletedresponse=cmclient.declare_task_done(picked_task_id, '007')
+
 print "deletedresponse = ", deletedresponse
 print "deletedresponse.text = ", deletedresponse.text
 
