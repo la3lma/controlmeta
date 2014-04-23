d360d3989876900bd5a095b9e3baa4533048654e
Bjørn Remseth <rmz@comoyo.com>
2014-04-23 17:30:17 +0200

    more consistent naming of variables.

--- tests/test-upload-task.py
+++ tests/test-upload-task.py
@@ -7,13 +7,13 @@ import client
 
 base_url=str(sys.argv[1])
 
-cmclient = client.ControlMetaClient(base_url)
+cmc = client.ControlMetaClient(base_url)
 
 print "Testing against base-url = ", base_url
 json_headers = {'content-type': 'application/json'}
 
 parameters={'parameter1':'Value 1'}
-data = cmclient.upload_task("face", parameters)
+data = cmc.upload_task("face", parameters)
 task_id=data['taskId']
 
 print "taskId=",task_id
@@ -21,18 +21,17 @@ print "taskId=",task_id
 ## Then pick a task
 
 print "Picking."
-pickresponse=cmclient.pick_task('face', '007')
+pickresponse=cmc.pick_task('face', '007')
 print "pickresponse = ", pickresponse
 picked_task_id=pickresponse['taskId']
 print "picked task id", picked_task_id
 
 
 ## And declare it as done
+doneresponse=cmc.declare_task_done(picked_task_id, '007')
 
-deletedresponse=cmclient.declare_task_done(picked_task_id, '007')
-
-print "deletedresponse = ", deletedresponse
-print "deletedresponse.text = ", deletedresponse.text
+print "doneresponse = ", doneresponse
+print "doneresponse.text = ", doneresponse.text
 
 
 
