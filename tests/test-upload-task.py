c8c90b38f2ff17454ba43df24e887ae5c1dd3915
Bjørn Remseth <rmz@comoyo.com>
2014-04-23 09:01:08 +0200

    Start building a client class.

--- tests/test-upload-task.py
+++ tests/test-upload-task.py
@@ -4,35 +4,44 @@ import sys
 import requests
 import json
 
+
+class  ControlMetaClient:
+    JSON_HEADERS = {'content-type': 'application/json'}
+
+    def __init__(self, base_url=None):
+      self.base_url=base_url
+
+    def upload_task(self, type, parameters):
+        tasktypepath = "task/type/%s" % type
+        posturl = "%s%s" %(base_url, tasktypepath)
+        print "posturl = ", posturl
+        response = requests.post(posturl, data=json.dumps(parameters), headers=self.JSON_HEADERS)
+        print response
+        print response.text
+        return json.loads(response.text)
+      
+    
 ## Parsing command line parameters
 # XXX Add some tests here to check the input parameter
 # print 'Number of arguments:', len(sys.argv), 'arguments.'
 # print 'Argument List:', str(sys.argv)
 base_url=str(sys.argv[1])
-tasktypepath="task/type/face"
+
+cmclient = ControlMetaClient(base_url)
 
 print "Testing against base-url = ", base_url
 json_headers = {'content-type': 'application/json'}
 
 
-def upload_task(type, parameters):
-    tasktypepath = "task/type/%s" % type
-    posturl = "%s%s" %(base_url, tasktypepath)
-    print "posturl = ", posturl
-    response = requests.post(posturl, data=json.dumps(parameters), headers=json_headers)
-    print response
-    print response.text
-    return json.loads(response.text)
 
 parameters={'parameter1':'Value 1'}
-
-
-data = upload_task("face", parameters)
+data = cmclient.upload_task("face", parameters)
 task_id=data['taskId']
 
 print "taskId=",task_id
 
 ## Then pick a task
+
 print "Picking."
 agent_id={'agentId':'007'}
 pickurl="%s%s" %(base_url, 'task/waiting/type/face/pick')
