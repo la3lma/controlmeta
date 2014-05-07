10bf587168202aef370a44abcf2fae4bbd3c1cf4
Bjørn Remseth <rmz@comoyo.com>
2014-05-07 17:13:09 +0200

    adding persistence test to alltests

--- tests-unit.sh
+++ tests-unit.sh
@@ -5,6 +5,7 @@ PYTHON=/usr/bin/python
 TESTS="test_empty_crud.py \
       test_media_and_meta_storage_directly.py \
       test_task_queue_storage.py \
+      test_media_proc_persistence.py \
       test_media_usecases.py \
       test_flask_task_manipulations.py "
 
