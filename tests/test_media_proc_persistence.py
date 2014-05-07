0db5c212775ac88e591731020f109eb947c023b9
Bjørn Remseth <rmz@comoyo.com>
2014-05-07 16:26:59 +0200

    More basic tests

--- tests/test_media_proc_persistence.py
+++ tests/test_media_proc_persistence.py
@@ -26,12 +26,37 @@ class SimpleCrudCases(unittest.TestCase):
         # create a configured "Session" class
         self.Session = sessionmaker(bind=self.engine)
 
-        # create a Session
-        self.session = self.Session()
-
     def tearDown(self):
         "Nothing to tear down yet"
         pass
+
+
+    def new_session(self):
+        self.session = self.Session()
+
+    def commit_session(self):
+        self.session.commit()
+
+
+    def store_new_task(self):
+        # First create the task
+        self.new_session()
+        new_task = Task(
+            runner = 'runner 1', 
+            tasktype = 'test task',
+            params = '{"foo":"bar"}')
+        self.session.add(new_task)
+        self.commit_session()
+        return new_task
+
+
+
+    def task_equal(self, t1, t2):
+        return  (t1.id == t2.id and
+                 t1.runner == t2.runner and
+                 t1.tasktype == t2.tasktype and
+                 t1.params == t2.params)
+
 
 
     ##
@@ -39,13 +64,17 @@ class SimpleCrudCases(unittest.TestCase):
     ## Start by using raw SQL manipulating commands, then co-evolve
     ## the tests into a persistence API that an be used by others tooﬁ
     ##
-    def test_store_task(self):
-        test_task = Task(
-            runner = 'runner 1', 
-            tasktype = 'test task',
-            params = '{"foo":"bar"}')
-        self.session.add(test_task)
-        self.session.commit()
+    def test_store_then_retrieve_task(self):
+        new_task = self.store_new_task()
+        # Get the ID of the task.
+        id = new_task.id
+        
+        # Then retrieve the task with that id
+        # Kick off a new session
+        self.session = self.Session()
+        retrieved_task = self.session.query(Task).get(id)
+
+        self.assertTrue(self.task_equal(new_task, retrieved_task))
         
 
     def test_delete_task(self):
