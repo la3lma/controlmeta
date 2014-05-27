050dd0e7f291ed2f16127390f491343e00282be1
Bjørn Remseth <rmz@comoyo.com>
2014-05-27 17:36:38 +0200

    Whitespace

--- database.py
+++ database.py
@@ -10,11 +10,8 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
 def init_db():
     Base.metadata.create_all(bind=engine)
-
-
 
 def commit_db():
     try:
