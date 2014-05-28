f53718e98362ce01c0d3ac6d624128f5524b22bb
Bjørn Remseth <rmz@comoyo.com>
2014-05-28 19:46:38 +0200

    Rig for unit tests

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # This works somewhat with acceptance tests, but fails some unit tests.
-SQLALCHEMY_DATABASE_URI='sqlite:///testdb.db'
+# SQLALCHEMY_DATABASE_URI='sqlite:///testdb.db'
 
 # This works perfectly with the unit tests, but fails
 # the acceptance tests.
-#SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
+SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
 
 # XXX This sensitivity is discuraging.
