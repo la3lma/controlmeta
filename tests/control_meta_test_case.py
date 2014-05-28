99d36cb20989a3f8c5049fee747572a85f7f7135
Bjørn Remseth <rmz@comoyo.com>
2014-05-28 19:46:02 +0200

    Fix bug, we now need to init the db for each run, so just do that.

--- tests/control_meta_test_case.py
+++ tests/control_meta_test_case.py
@@ -13,6 +13,8 @@ class Control_meta_test_case(unittest.TestCase):
     def setUp(self):
         "Talking to a temporary file database"
 
+        init_db()
+
         # Get the test client
         self.app = ctlm.app.test_client()
 
