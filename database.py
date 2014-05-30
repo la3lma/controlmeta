e5e1abdb0adef3bff4d9bcd92b4196dc481321f7
Bjørn Remseth <rmz@comoyo.com>
2014-05-30 11:19:00 +0200

    Add code to generate RDS connect string from info given in environment

--- database.py
+++ database.py
@@ -5,17 +5,41 @@ from sqlalchemy import Table, Column, Integer, String, MetaData
 import config
 import os
 
+# A very kludgy way of getting access to a database.
+# (refactor when shown to actually work)
+# First se see if there is an Amazon RDS instance available.
+# We're using the receipe suggested in 
+# http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_Python.rds.html
 
-# A kludgy way of gettingi config either from
-# an environment variable (as typically done during testing)
-# or from a config file (typically done during operation)
+def rds_connect_string(dbcfg):
+    db_uri  = dbcfg['ENGINE'] + "://"
+    db_uri += dbcfg['RDS_USERNAME'] + ":" +  dbcfg['RDS_PASSWORD']
+    db_uri += '@' + dbcfg['RDS_HOSTNAME'] + ':' + dbcfg['RDS_PORT']
+    db_uri += '/' + dbcfg['RDS_DB_NAME']
+    return db_uri
 
-try:
-    db_uri = os.environ["SQLALCHEMY_DATABASE_URI"]
-    print "Found environment variable"
-except KeyError:
-    print "Did not find environment variable"
-    db_uri = config.SQLALCHEMY_DATABASE_URI
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+    selected_db='default'
+    dbcfg=DATABASES[selected_db]
+    db_uri = rds_connect_string(dbcfg)
+
+else:
+    try:
+        db_uri = os.environ["SQLALCHEMY_DATABASE_URI"]
+        print "Found environment variable"
+    except KeyError:
+        print "Did not find environment variable"
+        db_uri = config.SQLALCHEMY_DATABASE_URI
 
 # Change to true to print all SQL statements going into and coming out of the database.
 echo=False
