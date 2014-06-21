e172dc556e94eb0aa02dbd3082736d768a3a6d76
Bjørn Remseth <rmz@comoyo.com>
2014-06-22 00:58:03 +0200

    Add test for correctly parsed full document

--- tests/test_imagemagic_identify_output_parser.py
+++ tests/test_imagemagic_identify_output_parser.py
@@ -157,14 +157,99 @@ class SimpleCrudCases(unittest.TestCase):
         result = self.parser.parse_lines(ls)
         self.assertEqual(parsed_image, result)
 
+
+    def test_parsing_imagemagic_output_from_static_file(self):
+        expected_result = \
+            { "Image" : 
+              { "Artifacts" : { "filename" : "tests/images/lena1.jpeg",
+                                "verbose" : "true"
+                                },
+                "Background color" : "white",
+                "Border color" : "srgb(223,223,223)",
+                "Channel depth" : { "blue" : "8-bit",
+                                    "green" : "8-bit",
+                                    "red" : "8-bit"
+                                    },
+                "Channel statistics" : { "Blue" : { "kurtosis" : "1.43816",
+                                                    "max" : "220 (0.862745)",
+                                                    "mean" : "68.011 (0.26671)",
+                                                    "min" : "0 (0)",
+                                                    "skewness" : "1.0309",
+                                                    "standard deviation" : "31.4847 (0.123469)"
+                                                    },
+                                         "Green" : { "kurtosis" : "0.140211",
+                                                     "max" : "232 (0.909804)",
+                                                     "mean" : "82.3407 (0.322905)",
+                                                     "min" : "0 (0)",
+                                                     "skewness" : "0.553692",
+                                                     "standard deviation" : "37.9124 (0.148676)"
+                                                     },
+                                         "Red" : { "kurtosis" : "-0.782758",
+                                                   "max" : "255 (1)",
+                                                   "mean" : "136.073 (0.53362)",
+                                                   "min" : "3 (0.0117647)",
+                                                   "skewness" : "-0.169742",
+                                                   "standard deviation" : "51.494 (0.201937)"
+                                                   }
+                                         },
+                "Chromaticity" : { "blue primary" : "(0.15,0.06)",
+                                   "green primary" : "(0.3,0.6)",
+                                   "red primary" : "(0.64,0.33)",
+                                   "white point" : "(0.3127,0.329)"
+                                   },
+                "Class" : "DirectClass",
+                "Colorspace" : "sRGB",
+                "Compose" : "Over",
+                "Compression" : "JPEG",
+                "Depth" : "8-bit",
+                "Dispose" : "Undefined",
+                "Elapsed time" : "0:01.009",
+                "Endianess" : "Undefined",
+                "Filesize" : "7.99KB",
+                "Format" : "JPEG (Joint Photographic Experts Group JFIF format)",
+                "Gamma" : "0.454545",
+                "Geometry" : "300x168+0+0",
+                "Image statistics" : { "Overall" : { "kurtosis" : "3.16837",
+                                                     "max" : "255 (1)",
+                                                     "mean" : "95.4749 (0.374411)",
+                                                     "min" : "0 (0)",
+                                                     "skewness" : "1.28073",
+                                                     "standard deviation" : "41.1512 (0.161377)"
+                                                     } },
+                "Intensity" : "Undefined",
+                "Interlace" : "None",
+                "Iterations" : "0",
+                "Matte color" : "grey74",
+                "Mime type" : "image/jpeg",
+                "Number pixels" : "50.4K",
+                "Orientation" : "Undefined",
+                "Page geometry" : "300x168+0+0",
+                "Pixels per second" : "5.04MB",
+                "Properties" : { "date:create" : "2014-05-27T22:32:43+02:00",
+                                 "date:modify" : "2014-05-27T22:32:43+02:00",
+                                 "jpeg:colorspace" : "2",
+                                 "jpeg:sampling-factor" : "2x2,1x1,1x1",
+                                 "signature" : "2596eb6ff77d0f1906f1923d0b7c98a352227fff88eab4cbb523dc26622d6e7f"
+                                 },
+                "Quality" : "74",
+                "Rendering intent" : "Perceptual",
+                "Tainted" : "False",
+                "Transparent color" : "black",
+                "Type" : "TrueColor",
+                "Units" : "Undefined",
+                "User time" : "0.000u",
+                "Version" : "ImageMagick 6.8.8-9 Q16 x86_64 2014-03-23 http://www.imagemagick.org"
+                } 
+              }
+        ls = FileLineSource("tests/imagemagick-identify-output.txt")
+        result = self.parser.parse_lines(ls)
+        self.assertEqual(expected_result, result)
+
+
 ##
 ## TODO
 ##
 
-    def test_parsing_imagemagic_output_from_static_file(self):
-        ls = FileLineSource("tests/imagemagick-identify-output.txt")
-        result = self.parser.parse_lines(ls)
-        print "Result = ", result
 
     def test_parsing_imagemagic_output_from_running_process(self):
         pass
