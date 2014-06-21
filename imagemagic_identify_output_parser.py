4b1a3b42ff9a90844d809b3c602f925a45f40ed3
Bjørn Remseth <rmz@comoyo.com>
2014-06-21 20:57:45 +0200

    Add tart of parser for image magick status output

---
+++ imagemagic_identify_output_parser.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import fileinput
+import re
+
+
+
+def parse_stdin():
+    for line in fileinput.input():
+        pass
+
+
+class Parser:
+
+
+    def get_indentation_of_line(self, txt):
+        m = re.match('^\s+', txt)
+        if not m:
+            return 0
+        else:
+            return len(m.group())
+
+    def split(self, txt):
+        index = self.get_indentation_of_line(txt)
+        return (index, txt[index:])
+
+
+    def parse_line_content(self, line):
+       "Parse the line, add to indentation if necessary"
+       m = re.match('^.+: ', line)
+       index = len(m.group())
+       tag = line[:index - 2]
+       content = line[index:]
+       return { tag : content }
+        
---
+++ tests/test_imagemagic_identify_output_parser.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Test parsing of output from imagemagick up to and including invoking
+    imagemagick on an actual file.
+
+    :copyright: (c) 2014 by Bjørn Remseth
+    :license: All rights reserved (at least for now)
+"""
+
+import os
+import unittest
+import json
+from  imagemagic_identify_output_parser import Parser
+
+
+class SimpleCrudCases(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = Parser()
+
+    def tearDown(self):
+        pass
+
+    def test_empty_string(self):
+        rv = self.parser.get_indentation_of_line('')
+        self.assertEqual(0, rv)
+
+    def test_string_with_no_leading_spaces(self):
+        rv = self.parser.get_indentation_of_line('xxx')
+        self.assertEqual(0, rv)
+
+    def test_string_with_one_leading_space(self):
+        rv = self.parser.get_indentation_of_line(' ')
+        self.assertEqual(1, rv)
+
+    def test_string_with_one_leading_space_followed_by_something(self):
+        rv = self.parser.get_indentation_of_line(' xxx')
+        self.assertEqual(1, rv)
+
+    def test_split_empty(self):
+        idx, txt = self.parser.split('')
+        self.assertEqual(0, idx)
+        self.assertEqual('', txt)
+
+
+    def test_split_leading_single_space(self):
+        idx, txt = self.parser.split(' ')
+        self.assertEqual(1, idx)
+        self.assertEqual('', txt)
+
+    def test_split_leading_single_space_followed_by_an_a(self):
+        idx, txt = self.parser.split(' a')
+        self.assertEqual(1, idx)
+        self.assertEqual('a', txt)
+
+
+    def test_parse_line_content_min(self):
+        r = self.parser.parse_line_content("min: 0 (0)")
+        self.assertEqual({"min": "0 (0)"}, r)
+
+
+if __name__ == '__main__':
+    unittest.main()
