c39f6b18f79ec2db731348dff6c81403bc0abc1c
Bjørn Remseth <rmz@comoyo.com>
2014-07-25 17:50:04 +0200

    Add the actual test that does the out-ractorization

---
+++ tests/test-availability-of-user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import client
+import logging
+import requests
+from requests.auth import HTTPDigestAuth
+from requests.auth import HTTPBasicAuth
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import errno, sys
+from get_test_auth import get_test_auth
+
+# Pick a base url from the command line
+base_url=str(sys.argv[1])
+
+# you need to initialize logging, 
+# otherwise you will not see anything from requests
+logging.basicConfig(level=logging.DEBUG) 
+
+# Get auth parameters from environment variables
+auth = get_test_auth()
+
+if not auth:
+    logging.debug("Could not get auth")
+    sys.exit(errno.ENOENT)
+
+# Then set up a client against that server
+cmc = client.ControlMetaClient(base_url, auth=auth)
+
+# Do we have any users? If so everything is ok, otherwise fail.
+users = cmc.get_users()
+logging.debug("All users = %r "% users)
+if not users:
+    sys.exit(errno.ENOENT)
