a5a58f2407511280e1315637b744444ede4ca9bd
Bjørn Remseth <rmz@comoyo.com>
2014-08-03 19:35:48 +0200

    use combined test suite instead of shell script iterating over individual tests.

--- tests-unit.sh
+++ tests-unit.sh
@@ -2,21 +2,14 @@
 clear
 
 PYTHON=/usr/bin/python
-TESTS="test_empty_crud.py \
-      test_media_and_meta_storage_directly.py \
-      test_mediametastorage.py \
-      test_task_queue_storage.py \
-      test_media_usecases.py \
-      test_database.py \
-      test_flask_task_manipulations.py \
-      test_userdb_crud.py"
+
 
 # Use the memory-only version of sqlite for
 # unit tests.
 export SQLALCHEMY_DATABASE_URI="sqlite:///:memory:"
 
-for test in $TESTS ; do
-  echo "======"
-  echo "Running tests in ${test}"
-  $PYTHON "tests/${test}"
-done
+test="unit_test_suite.py"
+echo "======"
+echo "Running tests in ${test}"
+$PYTHON "tests/${test}"
+
