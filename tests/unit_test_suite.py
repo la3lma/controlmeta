0db6d0ab265c11477d5c7610fe7276edb3e9c29c
Bjørn Remseth <rmz@comoyo.com>
2014-08-03 16:08:01 +0200

    Add some code to run the test as a suite. This may or may not be the canonical way of doing it but that's where we are now

---
+++ tests/unit_test_suite.py
@@ -0,0 +1,15 @@
+import unittest
+
+from test_empty_crud import SimpleCrudCases
+
+def suite():
+    new_suite = unittest.TestSuite()
+    new_suite.addTest(SimpleCrudCases())
+    return new_suite
+    #  s = unittest.TestLoader().loadTestsFromTestCase(SimpleCrudCases())
+    # return s
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    test_suite = suite()
+    runner.run (test_suite)
