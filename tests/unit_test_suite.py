28feaefdf50f131569876cd7be107e27c6b06b2b
Bjørn Remseth <rmz@comoyo.com>
2014-08-03 16:20:11 +0200

    An even more canonical way of making a suite

--- tests/unit_test_suite.py
+++ tests/unit_test_suite.py
@@ -1,17 +1,19 @@
 import unittest
 
 from test_empty_crud import SimpleCrudCases
+from test_flask_task_manipulations import FullTaskLifecycleTest
 
-def suite():
-    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleCrudCases)
-    return suite
-    # new_suite = unittest.TestSuite()
-    # new_suite.addTest(SimpleCrudCases())
-    # return new_suite
-    #  s = unittest.TestLoader().loadTestsFromTestCase(SimpleCrudCases())
-    # return s
+
+unit_tests = [SimpleCrudCases, FullTaskLifecycleTest]
+
+def suite(unit_tests):
+    new_suite = unittest.TestSuite()
+    for s in unit_tests :
+        tests = unittest.TestLoader().loadTestsFromTestCase(s)
+        new_suite.addTests(tests)
+    return new_suite
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
-    test_suite = suite()
+    test_suite = suite(unit_tests)
     runner.run (test_suite)
