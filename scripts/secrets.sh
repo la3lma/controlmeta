b9470cd741e9a601c45bb2ab17853624bbf6e542
Bjørn Remseth <rmz@comoyo.com>
2014-09-15 17:08:25 +0200

    mm

--- scripts/secrets.sh
+++
@@ -1,5 +0,0 @@
-
-# The master database in RDS.  This is a real secret, don't lose it or it could be
-# quite simple for someone to repurpose the database.
-
-export SQLALCHEMY_DATABASE_URI="postgresql://ctlmetamaster:foobarzz@aa12fev1azddbtc.cwp7kq9s0zsb.eu-west-1.rds.amazonaws.com:5432/ctlmeta"
