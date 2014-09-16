9e5c16ff2ed6727ffed6e31eff372a34dbd330fd
Bjørn Remseth <rmz@comoyo.com>
2014-09-16 18:41:22 +0200

    Progress towards a functioning build script.

--- docker/build.sh
+++ docker/build.sh
@@ -13,21 +13,25 @@ PRODUCTIONFILES="productionfiles.txt"
 TARGETDIR="$(PWD)/controlmeta"
 
 if [ -d "$TARGETDIR" ] ; then
-    echo rm -rf "$TARGETDIR"
+    rm -rf "$TARGETDIR"
 fi
 
 
 # Copy the files we need
 mkdir -p "$TARGETDIR"
-PRODUCTIONDIRS=$(cat "../$PRODUCTIONFILES" | grep '/' | awk -F/ '{print $1}' | sort | uniq)
-(cd "$TARGETDIR" && mkdir -p "$PRODUCTIONDIRS")
-(cd .. ; cp $(cat "$PRODUCTIONFILES") "$TARGETDIR" )
+
+for file in $(cat "../$PRODUCTIONFILES") ; do
+    DIRNAME="$(dirname $file)"
+    if [ -n "$DIRNAME" ] ; then
+	mkdir -p "$TARGETDIR/$DIRNAME"
+    fi
+    cp "../$file" "$TARGETDIR/$file"
+done
 
 
 # get the DOCKER_HOST environment variable (or fail&exit if unsuccessfull)
-
 . $BUILD_HOME_DIR/resolve-docker.sh
 
 # Then build the docker instance
-docker build .
+docker build --tag="controlmeta" .
 
